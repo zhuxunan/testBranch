4ec5b958448b9a023ba057151af0339cb8626baa
zhuxn@beagledata.com <zhuxn@beagledata.com>
2020-10-16 13:45:05 +0800

    初次提交

---
+++ src/main/java/com/example/testbranch/TestbranchApplication.java
@@ -0,0 +1,13 @@
+package com.example.testbranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestbranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestbranchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testbranch/TestbranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testbranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestbranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
